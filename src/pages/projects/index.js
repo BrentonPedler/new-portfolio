49147ff22f169ba74829542efafe903dd4ee1852
BrentonPedler <brenton_pedler@hotmail.com>
2022-11-11 11:40:23 -0500

    responsive update

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,4 +1,5 @@
 import { projectList } from './projectList';
+import { Box } from '@mui/material';
 import Grid from '@mui/material/Grid';
 
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -9,20 +10,35 @@ import style from './style.module.css'
 export default function Projects() {
     const renderProjects = projectList.map((data, key) => {
         return (
-            <div key={key} style={{ border: '2px solid #FFC23C', borderRadius: '25px', padding: '25px', margin: '50px 0px'}}>
-                <Grid container sx={{ textAlign: 'center' }}>
+            <Box 
+                key={key} 
+                className={style.projectContainer}
+                sx={{ padding: { xl: '25px', lg: '25px', md: '25px', sm: '15px', xs: '10px' } }}
+            >
+                <Grid 
+                    container 
+                    sx={{ textAlign: 'center' }}
+                >
                     <Grid item xs={12}>
                         <h2>{data.name}</h2>
                     </Grid>
                 </Grid>
                 <Grid container>
-                    <Grid item xs={12} sm={12} md={12} lg={12} xl={12} sx={{ textAlign: 'center'}}>
+                    <Grid 
+                        item 
+                        xs={12} sm={12} md={12} lg={12} xl={12} 
+                        sx={{ textAlign: 'center'}}
+                    >
                             <img src={data.picOne} alt={data.alt} className={style.img} />
                             <img src={data.picTwo} alt={data.alt} className={style.img} />
                     </Grid>
                 </Grid>
                 <Grid container>
-                    <Grid item item xs={12} sm={12} md={12} lg={12} xl={12} className={style.contentContainer}>
+                    <Grid 
+                        item 
+                        xs={12} sm={12} md={12} lg={12} xl={12} 
+                        className={style.contentContainer}
+                    >
                         <p>{data.description}</p>
                         <p>{data.tech}</p>
                         <a href={data.repo} target='_blank' rel="noopener noreferrer">
@@ -33,13 +49,16 @@ export default function Projects() {
                         </a>
                     </Grid>
                 </Grid>
-            </div>
+            </Box>
         )
     })
 
     return (
-        <div style={{ width: '80%', margin: '0 auto', display: 'flex', paddingBottom: '50px' }}>
-            <Grid container>
+        <div className={style.mainContainer}>
+            <Grid 
+                container
+                sx={{ width: { xl: '80%', lg: '80%', md: '80%', sm: '90%', xs: '95%' } }}
+            >
                 <Grid item xs={12}>
                     {renderProjects}
                 </Grid>
